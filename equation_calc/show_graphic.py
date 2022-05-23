183f9b2f7eb629f1f3ec98e02e355038083b580c
Leo Kazantsev <leokazantsev19@mail.ru>
2022-05-23 13:02:23 +0300

    updating

--- equation_calc/show_graphic.py
+++ equation_calc/show_graphic.py
@@ -5,7 +5,6 @@ def fig(*args):
       Fig = plt.figure(figsize=(1, 2))
       a = Fig.add_subplot(111)
       a.scatter(args[0], args[1], s=7, label="func", color=args[2], linewidth=2)
-      
       if (args[4] < args[0][-1]) and (args[4] > args[0][0]):
          a.scatter(args[4], args[5](args[4]), color="blue")
       if args[3]:
